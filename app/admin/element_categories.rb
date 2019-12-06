1aca372b1209194c428f49149bb27c1e147e033b
Raghu Betina <raghu@firstdraft.com>
2019-12-06 16:44:52 +0000

    Whitelist columns in ElementCategory for ActiveAdmin

--- app/admin/element_categories.rb
+++ app/admin/element_categories.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register ElementCategory do
+
+ permit_params :category_name, :position_number
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
