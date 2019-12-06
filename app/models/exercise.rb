2c40634a28f8f582ab9c57ead486e11e5de632f3
Raghu Betina <raghu@firstdraft.com>
2019-12-06 16:44:08 +0000

    Direct association between exercises and workout_exercises

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,6 +1,9 @@
 class Exercise < ApplicationRecord
   # Direct associations
 
+  has_many   :workout_exercises,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/workout_exercise.rb
+++ app/models/workout_exercise.rb
@@ -1,6 +1,8 @@
 class WorkoutExercise < ApplicationRecord
   # Direct associations
 
+  belongs_to :exercise
+
   belongs_to :workout,
              :counter_cache => true
 
