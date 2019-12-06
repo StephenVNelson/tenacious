f21c428cd1a7c7ae4bfef1b90de406562723e179
Raghu Betina <raghu@firstdraft.com>
2019-12-06 16:44:35 +0000

    Whitelist columns in Workout for ActiveAdmin

--- app/admin/workouts.rb
+++ app/admin/workouts.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Workout do
+
+ permit_params :user_id, :client_id, :scheduled_date, :logged_date, :phase_number, :week_number, :day_number, :workout_focus, :sets_reps_array, :resistance_array, :set_duration_array, :set_work_rest_array
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
